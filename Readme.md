6b39ee8de01a5d9d6e29efb28c7e0a4fedac2f93
Akash <mypc29281@gmail.com>
2026-08-15 21:27:27 +0530

    ATM Manager

---
+++ Readme.md
@@ -0,0 +1,8 @@
+        Essentional Things
+--------------------------------------
+1 Generate Pin
+2 Update Pin
+3 View Balance
+4 Deposite 
+5 Withdraw
+6 EXit
---
+++ app.py
@@ -0,0 +1,27 @@
+import random
+import string
+
+
+while True:
+
+    print("--------Welcome To 24x7 ATM------------")
+    print("Generate pin press 1: ")
+    print("Deposite Amount press 2: ")
+    print("Withdraw Amount press 3: ")
+    print("View Balance press 4: ")
+    print("Exit press 0:")
+    print("---------------------------------------")
+
+    user_input = input("Enter Your Choice: ")
+
+    #Genetate atm pin 
+    def generate_pin():
+        pin = random.randint(100000,999999)
+        return pin
+
+
+    if user_input == "1":
+        print(f"Your Pin: {generate_pin()}")
+
+
+
