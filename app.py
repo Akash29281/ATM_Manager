1c7a85b33dc3c38b1475929a8b2e021f31edbf44
Akash <mypc29281@gmail.com>
2026-08-18 23:04:05 +0530

    Fixing some bugs in withdraw function

--- app.py
+++ app.py
@@ -1,49 +1,59 @@
 import random
 
-deposit_Amt = 0
-Total_Amt = 0
+balance = 0
 
-
-#Genetate atm pin 
+# Generate ATM PIN
 def generate_pin():
-    pin = random.randint(100000,999999)
-    return pin
+    return random.randint(100000, 999999)
 
-#Withdraw
-def Withdraw():
-    user_withdraw = int(input("Enter Your Amount"))
-    Total_Amt = deposit_Amt - user_withdraw
-    print("Amount Withdraw Successfully !",user_withdraw)
-    print(Total_Amt)
+# Withdraw Amount
+def withdraw():
+    global balance
+
+    if balance == 0:
+        print("Account Balance is 0")
+        return
+
+    user_withdraw = int(input("Enter Your Amount: "))
+
+    if user_withdraw > balance:
+        print("Insufficient Balance!")
+    else:
+        balance -= user_withdraw
+        print("Amount Withdrawn Successfully!")
+        print("Available Balance:", balance)
+
 
 while True:
-    
 
-    print("--------Welcome To 24x7 ATM------------")
-    print("Generate pin press 1: ")
-    print("Deposite Amount press 2: ")
-    print("Withdraw Amount press 3: ")
-    print("View Balance press 4: ")
-    print("Exit press 0:")
+    print("\n--------Welcome To 24x7 ATM------------")
+    print("Generate PIN press 1")
+    print("Deposit Amount press 2")
+    print("Withdraw Amount press 3")
+    print("View Balance press 4")
+    print("Exit press 0")
     print("---------------------------------------")
 
     user_input = input("Enter Your Choice: ")
 
     if user_input == "1":
-        print(f"Your Pin: {generate_pin()}")
+        print(f"Your PIN: {generate_pin()}")
 
-    if user_input == "2":
-        user_dep = int(input("Enter Your amount: "))
-        deposit_Amt += user_dep
-        print("Deposite successfull !")
-        print(deposit_Amt)
+    elif user_input == "2":
+        user_dep = int(input("Enter Your Amount: "))
+        balance += user_dep
+        print("Deposit Successful!")
+        print("Available Balance:", balance)
 
-    if user_input == "3":
-        Withdraw()
+    elif user_input == "3":
+        withdraw()
 
-    if user_input == "4":
-        print("Avaiable Balance :",Total_Amt)
+    elif user_input == "4":
+        print("Available Balance:", balance)
 
     elif user_input == "0":
-        print("exit")
+        print("Exit")
         break
+
+    else:
+        print("Invalid Choice!")
