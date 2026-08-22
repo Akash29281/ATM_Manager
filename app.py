15f4b18860b0b0c701c7c0519d0593786a2c1920
Akash <mypc29281@gmail.com>
2026-08-22 21:02:31 +0530

    Fixing the bugs and adding User authintication

--- app.py
+++ app.py
@@ -1,34 +1,39 @@
 import random
 
+
+Attempt_count = 0
 balance = 0
 User_Pin = None
+
 # Generate ATM PIN
 def generate_pin():
     global User_Pin
     User_Pin = random.randint(100000, 999999)
     return User_Pin
 
-# Withdraw Amount
+# Withdraw Function
 def withdraw():
     global balance
-
+    global Attempt_count
+    user_withdraw = 0
     PIN = int(input("Enter PIN:"))
     if PIN != User_Pin:
         print("Incorrect Pin")
-        return
+        Attempt_count += 1
+        # print(Attempt_count)
+        if Attempt_count == 3:
+            print("UnAuthorized Person !")
+            return
     else:
+        user_withdraw = int(input("Enter Your Amount: "))
         if balance == 0:
             print("Account Balance is 0")
             return
-
-    user_withdraw = int(input("Enter Your Amount: "))
-
     if user_withdraw > balance:
         print("Insufficient Balance!")
     else:
         balance -= user_withdraw
         print("Amount Withdrawn Successfully!")
-
 
 while True:
 
@@ -45,16 +50,18 @@ while True:
     if user_input == "1":
         print(f"Your PIN:{generate_pin()}")
         pass
-
     #Deposite Function
     elif user_input == "2":
-        print(User_Pin)
         if User_Pin is None:
             print("Please generate a PIN first.")
             continue
         PIN = int(input("Enter PIN:"))
         if PIN != User_Pin:
             print("Incorrect Pin")
+            Attempt_count += 1
+            if Attempt_count == 3:
+                print("UnAuthorized Person !")
+                break
         else:
             user_dep = int(input("Enter Your Amount: "))
             if user_dep <= 0:
@@ -70,12 +77,15 @@ while True:
         PIN = int(input("Enter PIN:"))
         if PIN != User_Pin:
             print("Incorrect Pin")
+            Attempt_count += 1
+            if Attempt_count == 3:
+                print("UnAuthorized Person !")
+                break
         else:
             print("Available Balance:", balance)
     #Exit
     elif user_input == "0":
         print("Exit")
         break
-
     else:
         print("Invalid Choice!")
